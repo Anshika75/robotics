0faad4227b79ba295780f92f83f263e02c394bd4
Anshika <anshikathakur596@gmail.com>
2024-04-28 03:02:38 +0530

    lorem

--- script.js
+++ script.js
@@ -40,23 +40,23 @@ function theme(event) {
     landingcontent.innerHTML = `
         <h1><span style="color: cyan">Themes</span> <span style="color: red;">Across</span></h1>
         <div class="grid">
-            <a class="card">
+            <a href="https://www.technoxian.com/" class="card">
                 <img src="https://www.dropbox.com/scl/fi/oobkizj404268ahqcrhz0/icon_01.png?rlkey=xhfg4shtcb3m6dn0yvjpzr3vz&st=gfj00coe&dl=1" alt="robotics">
                 <h2>Innovation Quest</h2>
             </a>
-            <a class="card">
+            <a href="https://www.technoxian.com/" class="card">
                 <img src="https://www.dropbox.com/scl/fi/y6wezle9try83gincvcyt/icon_08.png?rlkey=zeujrdk564ney5bvxi2oc8eas&st=ztqzwgif&dl=1" alt="drone">
                 <h2>Robo Challenges</h2>
             </a>
-            <a class="card">
+            <a href="https://www.technoxian.com/" class="card">
                 <img src="https://www.dropbox.com/scl/fi/o51bphit4ank3xnso22l5/icon_10.png?rlkey=5wbpj5svjzyyz4sgmnbcuh375&st=fqichysr&dl=1" alt="rc">
                 <h2>Drone Adventures</h2>
             </a>
-            <a class="card">
+            <a href="https://www.technoxian.com/" class="card">
                 <img src="https://www.dropbox.com/scl/fi/zhhhu1rrmkzr93vp9giuk/icon_05.png?rlkey=z44dwnbc0hibfn7iq5ntwwfi2&st=orzdvmb0&dl=1" alt="stem">
                 <h2>RC Thrills</h2>
             </a>
-            <a class="card">
+            <a href="https://www.technoxian.com/" class="card">
                 <img src="https://www.dropbox.com/scl/fi/n0gjteveew5ha588hi39n/icon_03.png?rlkey=esr8venhd2ihdzr5is12v5pu5&st=tbmnyc5a&dl=1" alt="innovation">
                 <h2>STEM Challenges</h2>
             </a>
