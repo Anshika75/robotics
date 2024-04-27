6d2a2204f0ff0e517750eedf985a4b06ba191e74
Anshika <anshikathakur596@gmail.com>
2024-04-27 16:01:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var landingcontent = document.getElementById('landingcontent');
 
 function about(event) {
     landingcontent.classList.add("about");
+    landingcontent.classList.remove("theme");
     landingcontent.innerHTML = `
         <h1><span style="color: cyan">About</span> <span style="color: red;">Event</span></h1>
         <p>
@@ -18,6 +19,7 @@ function about(event) {
 }
 function home(event){
     landingcontent.classList.remove("about");
+    landingcontent.classList.remove("theme");
     landingcontent.innerHTML = `
     <h1><span style="color: cyan">TECH</span><span style="color: red;">NOXIAN</span></h1>
             <button class="button-49" role="button">Register Now</button>
@@ -26,5 +28,20 @@ function home(event){
                 <a href="">Themes</a> •
                 <a href="">Guidelines</a>
              </div>
+    `;
+}
+function theme(event) {
+    landingcontent.classList.add("theme");
+    landingcontent.innerHTML = `
+        <h1><span style="color: cyan">About</span> <span style="color: red;">Event</span></h1>
+        <p>
+            Technoxian is an internal competition held at Chandigarh University, offering students a platform to showcase their skills and creativity in various robotics and technology-related challenges. The competition aims to foster innovation, teamwork, and problem-solving abilities among participants.
+            There's going to be Wide Range of Competitions, Inclusive Participation, Hands-on Learning, Certifications, Prizes, Support and Guidance, Networking Opportunities and Promotion of Innovation.
+        </p>
+        <div class="menu">
+                <a  onclick="home()">Home</a> •
+                <a href="">Themes</a> •
+                <a href="">Guidelines</a>
+        </div>
     `;
 }
