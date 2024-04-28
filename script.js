fea78379d2a4341b2046a4b74a80f252e2933a47
Anshika <anshikathakur596@gmail.com>
2024-04-28 14:41:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ function about(event) {
     landingcontent.innerHTML = `
         <h1><span style="color: cyan">About</span> <span style="color: red;">Event</span></h1>
         <p>
-            Technoxian is an internal competition held at Chandigarh University, offering students a platform to showcase their skills and creativity in various robotics and technology-related challenges. The competition aims to foster innovation, teamwork, and problem-solving abilities among participants.
-            There's going to be Wide Range of Competitions, Inclusive Participation, Hands-on Learning, Certifications, Prizes, Support and Guidance, Networking Opportunities and Promotion of Innovation.
+            Technoxian is robot world competition held every year  for nominating teams from Chandigarh University provide a platform by organising internal competition offering students a platform to showcase their skills and creativity in various robotics and technology-related challenges. The competition aims to foster innovation, teamwork, and problem-solving abilities among participants. There's going to be Wide Range of Competitions, Inclusive Participation, Hands-on Learning, Certifications, Prizes, Support and Guidance, Networking Opportunities and Promotion of Innovation.
         </p>
         <div class="menu">
                 <a  onclick="home()">Home</a> •
